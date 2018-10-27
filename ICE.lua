2eb7ad235946eba2eebac06aad1d7e5848445e08
Benjamin Higgins <higgins.400@osu.edu>
2018-10-27 17:49:37 -0400

    Fuck Purple

---
+++ ICE.lua
@@ -0,0 +1,118 @@
+--File to create the scene for the ICE screen
+local composer = require( "composer" )
+local widget = require( "widget" )
+local localData = require("localData")
+local gamesparks = require("gamesparks")
+local scene = composer.newScene()
+ 
+-- -----------------------------------------------------------------------------------
+-- Code outside of the scene event functions below will only be executed ONCE unless
+-- the scene is removed entirely (not recycled) via "composer.removeScene()"
+-- -----------------------------------------------------------------------------------
+ 
+ 
+local w = display.actualContentWidth
+local h = display.actualContentHeight
+
+local function handleButtonEvent( event )
+    if (event.phase == "ended") then
+        print("First Condition")
+        if (event.target.id == "addButton") then
+            print("I Am Safe")
+            gamesparks.setStatus(1)
+        end
+        print(event.target.id .. " button pressed")
+    elseif (event.phase == "began") then
+        event.target:setFillColor(0,0,1)
+        print("begin")
+    end
+end
+
+local function inputListener( event )
+    
+   
+end
+
+
+-- -----------------------------------------------------------------------------------
+-- Scene event functions
+-- -----------------------------------------------------------------------------------
+
+-- create()
+function scene:create( event )
+ 
+    local sceneGroup = self.view
+    -- Code here runs when the scene is first created but has not yet appeared on screen
+ 
+end
+ 
+ 
+-- show()
+function scene:show( event )
+ 
+    local sceneGroup = self.view
+    local phase = event.phase
+ 
+    if ( phase == "will" ) then
+        -- Code here runs when the scene is still off screen (but is about to come on screen)
+ 
+    elseif ( phase == "did" ) then
+        -- Code here runs when the scene is entirely on screen
+
+        local addButton = widget.newButton({
+            id = "addButton",
+            fillColor = { default={ 138, 43, 226 }, over={ 138, 43, 226 } },
+            x = w / 2,
+            y = h * .8,
+            width = w/1.4,
+            height = 2 * (h/20),
+            label = "Add ICE",
+            fontSize = h/20,
+            shape = "roundedRect",
+            cornerRadius = (h/20) * 2 / 3,
+            labelColor = { default={ 0, 0, 0 }, over={ 0, 0, 0 } },
+            onEvent = handleButtonEvent,
+            --onRelease = btnPressed
+        })
+
+        sceneGroup:insert( addButton )
+        --addButton.labelColor = { default={ 0, 200, 0 }, over={ 0, 0, 0, 0.5 } }
+    end
+end
+ 
+ 
+-- hide()
+function scene:hide( event )
+ 
+    local sceneGroup = self.view
+    local phase = event.phase
+ 
+    if ( phase == "will" ) then
+        -- Code here runs when the scene is on screen (but is about to go off screen)
+ 
+    elseif ( phase == "did" ) then
+        -- Code here runs immediately after the scene goes entirely off screen
+ 
+    end
+end
+ 
+ 
+-- destroy()
+function scene:destroy( event )
+ 
+    local sceneGroup = self.view
+    -- Code here runs prior to the removal of scene's view
+ 
+end
+ 
+ 
+-- -----------------------------------------------------------------------------------
+-- Scene event function listeners
+-- -----------------------------------------------------------------------------------
+scene:addEventListener( "create", scene )
+scene:addEventListener( "show", scene )
+scene:addEventListener( "hide", scene )
+scene:addEventListener( "destroy", scene )
+-- -----------------------------------------------------------------------------------
+ 
+return scene
