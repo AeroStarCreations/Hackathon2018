e94024584b56e00a8e7f33b8e54e4e0bf36d2448
Sam Coyle <samcoyle97@yahoo.com>
2018-10-27 14:32:01 -0400

    Added Home Page

---
+++ home.lua
@@ -0,0 +1,140 @@
+--File to create the scene for the login screen
+local composer = require( "composer" )
+local widget = require( "widget" )
+local localData = require("localData")
+local gamesparks = require("gamesparks")
+local scene = composer.newScene()
+ 
+-- -----------------------------------------------------------------------------------
+-- Code outside of the scene event functions below will only be executed ONCE unless
+-- the scene is removed entirely (not recycled) via "composer.removeScene()"
+-- -----------------------------------------------------------------------------------
+ 
+ 
+local w = display.actualContentWidth
+local h = display.actualContentHeight
+local username
+local password
+local displayName
+
+
+local function handleButtonEvent( event )
+    if (event.phase == "ended") then
+        if (event.target.id == "login") then
+            if (username.text == nil or username.text == "" or
+            password.text == nil or password.text == "") then
+                print( "Must fill all fields" )
+                --infoClear()
+                --infoUpdate( "Must fill all fields" )
+            else 
+                if localData.isRegistered then
+                    print("Is Registered")
+                    gamesparks.loginWithUsernameAndPassword( 
+                        username.text,
+                        password.text
+                    )
+
+                else
+                    print("Is Not Registered")
+                    gamesparks.registerWithUsernameAndPassword(
+                        displayName.text,
+                        username.text, 
+                        password.text
+                    )
+                    localData.setPassword(password.text)
+                    localData.setUsername(username.text)
+                    localData.setRegistered(true)
+                end
+            end
+            native.setKeyboardFocus( nil )
+        elseif (event.target.id == "back") then
+            composer.gotoScene( composer.getSceneName( "previous" ))
+        end
+        print(event.target.id .. " button pressed")
+    end
+end
+
+local function inputListener( event )
+    
+   
+end
+
+
+-- -----------------------------------------------------------------------------------
+-- Scene event functions
+-- -----------------------------------------------------------------------------------
+
+-- create()
+function scene:create( event )
+ 
+    local sceneGroup = self.view
+    -- Code here runs when the scene is first created but has not yet appeared on screen
+ 
+end
+ 
+ 
+-- show()
+function scene:show( event )
+ 
+    local sceneGroup = self.view
+    local phase = event.phase
+ 
+    if ( phase == "will" ) then
+        -- Code here runs when the scene is still off screen (but is about to come on screen)
+ 
+    elseif ( phase == "did" ) then
+        -- Code here runs when the scene is entirely on screen
+
+        local safeButton = widget.newButton({
+            id = "safeButton",
+            x = w / 2,
+            y = h * .4,
+            width = w/1.4,
+            height = 2 * username.height,
+            label = "I'M SAFE",
+            fontSize = username.height,
+            shape = "roundedRect",
+            cornerRadius = username.height * 2 / 3,
+            onEvent = handleButtonEvent,
+        })
+
+        safeButton.labelColor = { default={ 0, 200, 0 }, over={ 0, 0, 0, 0.5 } }
+    end
+end
+ 
+ 
+-- hide()
+function scene:hide( event )
+ 
+    local sceneGroup = self.view
+    local phase = event.phase
+ 
+    if ( phase == "will" ) then
+        -- Code here runs when the scene is on screen (but is about to go off screen)
+ 
+    elseif ( phase == "did" ) then
+        -- Code here runs immediately after the scene goes entirely off screen
+ 
+    end
+end
+ 
+ 
+-- destroy()
+function scene:destroy( event )
+ 
+    local sceneGroup = self.view
+    -- Code here runs prior to the removal of scene's view
+ 
+end
+ 
+ 
+-- -----------------------------------------------------------------------------------
+-- Scene event function listeners
+-- -----------------------------------------------------------------------------------
+scene:addEventListener( "create", scene )
+scene:addEventListener( "show", scene )
+scene:addEventListener( "hide", scene )
+scene:addEventListener( "destroy", scene )
+-- -----------------------------------------------------------------------------------
+ 
+return scene
